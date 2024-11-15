b40173ac7ea6ba0a71814e9ff124688730cdd536
THS <ths@lmis.de>
2024-11-15 15:00:25 +0100

    - initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import OpenLayersComponent from "./components/OpenLayersComponent/OpenLayersComponent";
+
+function App() {
+  return (<OpenLayersComponent id="map"/>);
+}
+
+export default App;
