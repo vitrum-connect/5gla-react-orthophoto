7e248c3d8fe35c94e81e1b7ed8d570a152981fd2
THS-LMIS <91137979+THS-LMIS@users.noreply.github.com>
2024-11-26 08:34:48 +0100

    #1 Aufsetzen - Project Settings (#2)

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import OpenLayersComponent from "./components/OpenLayersComponent/OpenLayersComponent";
+
+function App() {
+  return (<OpenLayersComponent id="map"/>);
+}
+
+export default App;
---
+++ src/components/OpenLayersComponent/OpenLayersComponent.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+import { Map, View } from 'ol';
+
+import 'ol/ol.css';
+import './OpenLayersComponent.css';
+
+interface Props {
+    id: string
+}
+
+export default function OpenLayersComponent({ id }: Props) {
+    useEffect(() => {
+        const osmLayer = new TileLayer({
+            preload: Infinity,
+            source: new OSM(),
+        })
+
+        const map = new Map({
+            target: id,
+            layers: [ osmLayer ],
+            view: new View({
+                center: [0, 0],
+                zoom: 0,
+            }),
+        });
+        return () => map.setTarget(undefined)
+    });
+
+    return (<div id={id} className="map"></div>);
+}
